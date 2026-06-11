8a961b1e2bbc6fe2afa183c2044e25711f747041
kaban123123 <valera11122212@mail.ru>
2026-06-12 02:32:43 +0300

    Update main.py

--- main.py
+++ main.py
@@ -122,11 +122,8 @@ def analyze_image_with_logmeal(image_bytes: bytes) -> str:
                 lines.append(f"Не удалось получить нутриенты: {nutri_response.status_code}")
                 lines.append(nutri_response.text)
 
-        lines.append("")
-        lines.append("Если хотите, могу добавить оценку под цель: похудение / поддержание / набор.
-
-
-)
+       lines.append("")
+        lines.append("Если хотите, могу добавить оценку под цель: похудение / поддержание / набор.")
         return "\n".join(lines)
 
     except Exception as e:
